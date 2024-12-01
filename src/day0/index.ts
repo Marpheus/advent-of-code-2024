5388e7577fa66d195331f256f47e94787f7b3a02
Maros Vasilisin <maros.vasilisin@futured.app>
2024-12-01 14:13:09 +0100

    init

---
+++ src/day0/index.ts
@@ -0,0 +1,20 @@
+import { Day } from "../day";
+import fs from "fs";
+import * as readline from "readline";
+
+class Day1 extends Day {
+
+    constructor(){
+        super(1);
+    }
+
+    solveForPartOne(input: string[]): string {
+        return ''
+    }
+
+    solveForPartTwo(input: string[]): string {
+        return ''
+    }
+}
+
+export default new Day1;
---
+++ src/day1/index.ts
@@ -0,0 +1,23 @@
+import { Day } from "../day";
+import fs from "fs";
+import * as readline from "readline";
+
+class Day1 extends Day {
+
+    constructor(){
+        super(1);
+    }
+
+    solveForPartOne(input: string[]): string {
+        let sum = 0
+
+        return sum.toString()
+    }
+
+    solveForPartTwo(input: string[]): string {
+        let sum = 0
+        return sum.toString()
+    }
+}
+
+export default new Day1;
