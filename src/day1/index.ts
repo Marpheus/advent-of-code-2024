6a92768d8fc8caf90d781fd449f596405f4ae40d
Maros Vasilisin <maros.vasilisin@futured.app>
2024-12-01 14:24:45 +0100

    day1

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,6 +1,4 @@
-import { Day } from "../day";
-import fs from "fs";
-import * as readline from "readline";
+import {Day} from "../day";
 
 class Day1 extends Day {
 
@@ -11,11 +9,52 @@ class Day1 extends Day {
     solveForPartOne(input: string[]): string {
         let sum = 0
 
+        let lefts: string[] = []
+        let rights: string[] = []
+
+        input.forEach((line) => {
+            const [left, right] = line.split('   ')
+            lefts.push(left.trim())
+            rights.push(right.trim())
+        })
+
+        lefts = lefts.sort()
+        rights = rights.sort()
+
+        for (let i = 0; i < lefts.length; i++) {
+            const distance = Math.abs(parseInt(lefts[i]) - parseInt(rights[i]))
+            sum += distance
+        }
+
         return sum.toString()
     }
 
     solveForPartTwo(input: string[]): string {
         let sum = 0
+        let lefts: string[] = []
+        let rights: string[] = []
+
+        input.forEach((line) => {
+            const [left, right] = line.split('   ')
+            lefts.push(left.trim())
+            rights.push(right.trim())
+        })
+
+        lefts = lefts.sort()
+        rights = rights.sort()
+
+
+        for (let i = 0; i < lefts.length; i++) {
+
+            let score = 0
+            for (let j = 0; j < rights.length; j++) {
+                if (lefts[i] === rights[j]) {
+                    score += 1
+                }
+            }
+            sum += score * parseInt(lefts[i])
+        }
+
         return sum.toString()
     }
 }
